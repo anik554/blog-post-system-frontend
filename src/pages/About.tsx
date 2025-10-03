6c225a81f5e7bd6cfd3f03c399c4c5fde9f1ca1e
anik924 <anikimtiaz924@gmail.com>
2025-10-03 16:29:07 +0600

    theme setting and dark mode setup

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <div>About</div>
+      <div>
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores
+        aperiam veniam ab laboriosam hic fugit omnis consequuntur repellat harum
+        laudantium optio eligendi, necessitatibus voluptatibus laborum
+        distinctio incidunt quisquam doloremque libero provident odio error.
+        Earum dolorem inventore eius, in nobis, nisi est ad consequatur vitae
+        tempora dolores assumenda quibusdam corporis suscipit. Cumque eveniet
+        omnis, qui, dolor obcaecati, autem hic sunt consequuntur repellat nam
+        cupiditate. Ratione quas, quis veniam numquam cupiditate eveniet? Porro
+        doloremque harum dolor incidunt est nobis non quae deserunt recusandae.
+        Voluptates dolor, ratione harum aliquid cum, expedita quisquam tempora
+        nulla accusamus sapiente, qui fugiat reprehenderit iure neque illo
+        doloribus hic architecto consequuntur! Officiis, facilis! Ipsum eligendi
+        mollitia voluptates temporibus, perferendis facere in laborum maiores
+        sint quae sapiente aut nulla sunt, reiciendis recusandae soluta
+        delectus, possimus quidem eveniet voluptas. Pariatur consequatur
+        corrupti asperiores, laborum sapiente, dicta ea quam eos voluptatem
+        obcaecati illo! Incidunt voluptate nam dolor ut sequi veniam laborum
+        eveniet, error fugit. Maiores velit, placeat nam nesciunt voluptatibus
+        est odit in rem molestias ab corporis sed eius temporibus saepe facere
+        excepturi itaque explicabo nemo commodi similique eaque cumque. Cum
+        maxime accusantium distinctio aspernatur illo, error aliquid optio
+        autem? Laboriosam, ad? Nulla quis est aut veritatis veniam a ullam
+        voluptate amet ipsa quidem saepe fugiat ratione eaque culpa officia quo
+        eos perspiciatis distinctio architecto, rerum maxime ex accusantium
+        harum rem? Modi ipsum tempora quae autem consequuntur atque nemo
+        sapiente optio provident vitae, animi quidem sunt repudiandae doloribus
+        nam error repellendus eveniet quia expedita quasi, consectetur in! Ab
+        labore provident voluptates, porro error molestiae, ratione officiis
+        modi facere temporibus eos beatae ipsa magnam dolore eius dolorum magni
+        placeat at nam enim itaque deserunt ipsum tempore? Consequuntur autem
+        cum eius perspiciatis quos, aspernatur alias qui culpa dignissimos modi,
+        fuga, aliquam voluptatum ullam eveniet reprehenderit sapiente fugiat. At
+        cumque cupiditate ipsa vero quo ullam accusamus quis architecto, vel
+        tenetur eligendi distinctio itaque debitis, unde quidem aspernatur
+        veniam corrupti, possimus ducimus a officiis porro quas reiciendis.
+        Tempora aspernatur optio eum distinctio incidunt reprehenderit
+        temporibus eius inventore! Totam, fugiat facilis. Modi possimus libero
+        repellat, hic sint neque saepe unde consequatur, et voluptatibus
+        impedit, illo laborum animi illum. Iure animi culpa asperiores est
+        provident. Consectetur fugit nobis similique rerum nulla rem, impedit
+        quasi, perferendis minus nam placeat facere doloribus expedita modi
+        numquam laboriosam pariatur quo voluptatem quisquam iste harum a
+        dignissimos obcaecati dolorum! Similique magnam quidem nesciunt earum,
+        perferendis veniam at cumque dolor. Consequuntur, eaque. Voluptates non
+        a, et labore cupiditate voluptatibus rem officiis? Dignissimos, aperiam!
+        Quo officiis quis esse recusandae, non blanditiis qui explicabo dolorem
+        exercitationem? Ex molestias aut ut iste delectus voluptas suscipit
+        perferendis et mollitia, ratione temporibus esse architecto dignissimos
+        amet nobis corporis quia neque sunt recusandae! Ea, quasi veniam
+        deserunt ipsum animi labore, neque delectus cupiditate nobis
+        reprehenderit porro repellat placeat, ratione fuga atque!
+        Necessitatibus, quo consequuntur molestiae porro odio beatae et,
+        exercitationem excepturi accusamus, illo praesentium dicta quis in
+        aspernatur sapiente nostrum perferendis commodi quod sint. Ducimus rem
+        commodi voluptates sapiente accusamus, consequatur nobis in, eius
+        distinctio, enim quia vitae! Sint.{" "}
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
