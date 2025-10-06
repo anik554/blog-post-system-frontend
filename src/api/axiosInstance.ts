ea96361f51f3708e72440f706bc96b255f9b9a9c
anik924 <anikimtiaz924@gmail.com>
2025-10-06 23:18:43 +0600

    update

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://blog-post-backend-p1u2dwt3n-aniks-projects-d10b87e1.vercel.app/api/v1",
+  baseURL: "https://blog-post-backend-ten.vercel.app/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -1,1 +1,1 @@
-export const baseUrl = "https://blog-post-backend-p1u2dwt3n-aniks-projects-d10b87e1.vercel.app/api/v1"
+export const baseUrl = "https://blog-post-backend-ten.vercel.app/api/v1"
